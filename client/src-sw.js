141dd140b19b9e9e68df8c54b715f2dec5327af6
Michael Virga <michael@studiomav.com>
2023-03-09 00:44:01 -0800

    webpack, src-sw

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -7,24 +7,47 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
+const pageCache = new CacheFirst(
+  {
+    cacheName: 'page-cache',
+    plugins:
+      [
+        new CacheableResponsePlugin(
+          {
+            statuses: [0, 200],
+          }),
+        new ExpirationPlugin(
+          {
+            maxAgeSeconds: 30 * 24 * 60 * 30
+          }),
+      ],
+  });
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
+warmStrategyCache(
+  {
+    urls: ['/index.html', '/'],
+    strategy: pageCache,
+  });
+
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate(
+    {
+      cacheName: 'asset-cache',
+      plugins:
+        [
+          new CacheableResponsePlugin(
+            {
+              statuses: [0, 200],
+            }),
+          new ExpirationPlugin(
+            {
+              maxEntries: 60,
+              maxAgeSeconds: 30 * 24 * 60 * 30
+            })
+        ],
+    })
+);
